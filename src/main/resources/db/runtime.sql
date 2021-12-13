fc20757a7658534c03b1db6fc4733e5152e8bb0c
Guillaume GALLOIS <guillaume.gallois@gmail.com>
2021-12-13 17:44:04 +0100

    Update runtime.sql

--- src/main/resources/db/runtime.sql
+++ src/main/resources/db/runtime.sql
@@ -1,2 +1,3 @@
 insert into coffee values (1, 'Red Mountain', 'Europe', 'Light');
 insert into coffee values (2, 'Yellow Mountain', 'Lebanon', 'Strong');
+insert into coffee values (3, 'Red tall Mountain', 'Lebanon', 'Strong');
